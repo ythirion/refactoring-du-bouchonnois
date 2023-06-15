f1fbe7d6fc16d2bc3d86ef3808ac95ae8733f7d0
Yoan Thirion <yoan.thirion@gmail.com>
2023-06-15 20:52:57 +0400

    add Approval for ConsulterStatus

--- Bouchonnois.Tests/Unit/ConsulterStatus.cs
+++ Bouchonnois.Tests/Unit/ConsulterStatus.cs
@@ -3,93 +3,58 @@ using Bouchonnois.Service.Exceptions;
 
 namespace Bouchonnois.Tests.Unit
 {
+    [UsesVerify]
     public class ConsulterStatus : PartieDeChasseServiceTest
     {
         [Fact]
-        public void QuandLaPartieVientDeDémarrer()
+        public Task QuandLaPartieVientDeDémarrer()
         {
-            Given(
-                UnePartieDeChasseExistante(
-                    SurUnTerrainRicheEnGalinettes()
-                        .Avec(Dédé(), Bernard(), Robert().AyantTué(2))
-                        .Events(new Event(new DateTime(2024, 4, 25, 9, 0, 12),
-                            "La partie de chasse commence à Pitibon sur Sauldre avec Dédé (20 balles), Bernard (8 balles), Robert (12 balles)"))
-                )
-            );
+            var id = UnePartieDeChasseExistante(
+                SurUnTerrainRicheEnGalinettes()
+                    .Avec(Dédé(), Bernard(), Robert().AyantTué(2))
+                    .Events(new Event(new DateTime(2024, 4, 25, 9, 0, 12),
+                        "La partie de chasse commence à Pitibon sur Sauldre avec Dédé (20 balles), Bernard (8 balles), Robert (12 balles)"))
+            ).Id;
 
-            string? status = null;
-            When(id => status = PartieDeChasseService.ConsulterStatus(id));
-
-            Then(() => status.Should()
-                .Be(
-                    "09:00 - La partie de chasse commence à Pitibon sur Sauldre avec Dédé (20 balles), Bernard (8 balles), Robert (12 balles)")
-            );
+            return Verify(PartieDeChasseService.ConsulterStatus(id))
+                .DontScrubDateTimes();
         }
 
         [Fact]
-        public void QuandLaPartieEstTerminée()
+        public Task QuandLaPartieEstTerminée()
         {
-            Given(
-                UnePartieDeChasseExistante(
-                    SurUnTerrainRicheEnGalinettes()
-                        .Avec(Dédé(), Bernard(), Robert().AyantTué(2))
-                        .Events(
-                            new Event(new DateTime(2024, 4, 25, 9, 0, 12),
-                                "La partie de chasse commence à Pitibon sur Sauldre avec Dédé (20 balles), Bernard (8 balles), Robert (12 balles)"),
-                            new Event(new DateTime(2024, 4, 25, 9, 10, 0), "Dédé tire"),
-                            new Event(new DateTime(2024, 4, 25, 9, 40, 0), "Robert tire sur une galinette"),
-                            new Event(new DateTime(2024, 4, 25, 10, 0, 0), "Petit apéro"),
-                            new Event(new DateTime(2024, 4, 25, 11, 0, 0), "Reprise de la chasse"),
-                            new Event(new DateTime(2024, 4, 25, 11, 2, 0), "Bernard tire"),
-                            new Event(new DateTime(2024, 4, 25, 11, 3, 0), "Bernard tire"),
-                            new Event(new DateTime(2024, 4, 25, 11, 4, 0), "Dédé tire sur une galinette"),
-                            new Event(new DateTime(2024, 4, 25, 11, 30, 0), "Robert tire sur une galinette"),
-                            new Event(new DateTime(2024, 4, 25, 11, 40, 0), "Petit apéro"),
-                            new Event(new DateTime(2024, 4, 25, 14, 30, 0), "Reprise de la chasse"),
-                            new Event(new DateTime(2024, 4, 25, 14, 41, 0), "Bernard tire"),
-                            new Event(new DateTime(2024, 4, 25, 14, 41, 1), "Bernard tire"),
-                            new Event(new DateTime(2024, 4, 25, 14, 41, 2), "Bernard tire"),
-                            new Event(new DateTime(2024, 4, 25, 14, 41, 3), "Bernard tire"),
-                            new Event(new DateTime(2024, 4, 25, 14, 41, 4), "Bernard tire"),
-                            new Event(new DateTime(2024, 4, 25, 14, 41, 5), "Bernard tire"),
-                            new Event(new DateTime(2024, 4, 25, 14, 41, 6), "Bernard tire"),
-                            new Event(new DateTime(2024, 4, 25, 14, 41, 7),
-                                "Bernard tire -> T'as plus de balles mon vieux, chasse à la main"),
-                            new Event(new DateTime(2024, 4, 25, 15, 0, 0), "Robert tire sur une galinette"),
-                            new Event(new DateTime(2024, 4, 25, 15, 30, 0),
-                                "La partie de chasse est terminée, vainqueur :  Robert - 3 galinettes")
-                        )
-                )
-            );
+            var id = UnePartieDeChasseExistante(
+                SurUnTerrainRicheEnGalinettes()
+                    .Avec(Dédé(), Bernard(), Robert().AyantTué(2))
+                    .Events(
+                        new Event(new DateTime(2024, 4, 25, 9, 0, 12),
+                            "La partie de chasse commence à Pitibon sur Sauldre avec Dédé (20 balles), Bernard (8 balles), Robert (12 balles)"),
+                        new Event(new DateTime(2024, 4, 25, 9, 10, 0), "Dédé tire"),
+                        new Event(new DateTime(2024, 4, 25, 9, 40, 0), "Robert tire sur une galinette"),
+                        new Event(new DateTime(2024, 4, 25, 10, 0, 0), "Petit apéro"),
+                        new Event(new DateTime(2024, 4, 25, 11, 0, 0), "Reprise de la chasse"),
+                        new Event(new DateTime(2024, 4, 25, 11, 2, 0), "Bernard tire"),
+                        new Event(new DateTime(2024, 4, 25, 11, 3, 0), "Bernard tire"),
+                        new Event(new DateTime(2024, 4, 25, 11, 4, 0), "Dédé tire sur une galinette"),
+                        new Event(new DateTime(2024, 4, 25, 11, 30, 0), "Robert tire sur une galinette"),
+                        new Event(new DateTime(2024, 4, 25, 11, 40, 0), "Petit apéro"),
+                        new Event(new DateTime(2024, 4, 25, 14, 30, 0), "Reprise de la chasse"),
+                        new Event(new DateTime(2024, 4, 25, 14, 41, 0), "Bernard tire"),
+                        new Event(new DateTime(2024, 4, 25, 14, 41, 1), "Bernard tire"),
+                        new Event(new DateTime(2024, 4, 25, 14, 41, 2), "Bernard tire"),
+                        new Event(new DateTime(2024, 4, 25, 14, 41, 3), "Bernard tire"),
+                        new Event(new DateTime(2024, 4, 25, 14, 41, 4), "Bernard tire"),
+                        new Event(new DateTime(2024, 4, 25, 14, 41, 5), "Bernard tire"),
+                        new Event(new DateTime(2024, 4, 25, 14, 41, 6), "Bernard tire"),
+                        new Event(new DateTime(2024, 4, 25, 14, 41, 7),
+                            "Bernard tire -> T'as plus de balles mon vieux, chasse à la main"),
+                        new Event(new DateTime(2024, 4, 25, 15, 0, 0), "Robert tire sur une galinette"),
+                        new Event(new DateTime(2024, 4, 25, 15, 30, 0),
+                            "La partie de chasse est terminée, vainqueur :  Robert - 3 galinettes")
+                    )).Id;
 
-            string? status = null;
-            When(id => status = PartieDeChasseService.ConsulterStatus(id));
-
-            Then(() =>
-                status.Should()
-                    .BeEquivalentTo(
-                        @"15:30 - La partie de chasse est terminée, vainqueur :  Robert - 3 galinettes
-15:00 - Robert tire sur une galinette
-14:41 - Bernard tire -> T'as plus de balles mon vieux, chasse à la main
-14:41 - Bernard tire
-14:41 - Bernard tire
-14:41 - Bernard tire
-14:41 - Bernard tire
-14:41 - Bernard tire
-14:41 - Bernard tire
-14:41 - Bernard tire
-14:30 - Reprise de la chasse
-11:40 - Petit apéro
-11:30 - Robert tire sur une galinette
-11:04 - Dédé tire sur une galinette
-11:03 - Bernard tire
-11:02 - Bernard tire
-11:00 - Reprise de la chasse
-10:00 - Petit apéro
-09:40 - Robert tire sur une galinette
-09:10 - Dédé tire
-09:00 - La partie de chasse commence à Pitibon sur Sauldre avec Dédé (20 balles), Bernard (8 balles), Robert (12 balles)"
-                    ));
+            return Verify(PartieDeChasseService.ConsulterStatus(id))
+                .DontScrubDateTimes();
         }
 
         public class Echoue : PartieDeChasseServiceTest
