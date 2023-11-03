f60b6d2f2f2de32bc256eeabbc1dedeb73f858f5
Yoan Thirion <yoan.thirion@gmail.com>
2023-11-03 14:59:28 +0100

    finish démarrer

--- kotlin/refactoring-du-bouchonnois/src/test/kotlin/bouchonnois/tests/service/PartieDeChasseServiceTests.kt
+++ kotlin/refactoring-du-bouchonnois/src/test/kotlin/bouchonnois/tests/service/PartieDeChasseServiceTests.kt
@@ -2,7 +2,11 @@ package bouchonnois.tests.service
 
 import bouchonnois.domain.PartieStatus
 import bouchonnois.service.PartieDeChasseService
+import bouchonnois.service.exceptions.ImpossibleDeDémarrerUnePartieAvecUnChasseurSansBalle
+import bouchonnois.service.exceptions.ImpossibleDeDémarrerUnePartieSansChasseur
+import bouchonnois.service.exceptions.ImpossibleDeDémarrerUnePartieSansGalinettes
 import bouchonnois.tests.doubles.PartieDeChasseRepositoryForTests
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FeatureSpec
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
@@ -40,6 +44,53 @@ class PartieDeChasseServiceTests : FeatureSpec({
             savedPartieDeChasse.chasseurs!![2].nom shouldBe "Robert"
             savedPartieDeChasse.chasseurs!![2].ballesRestantes shouldBe 12
             savedPartieDeChasse.chasseurs!![2].nbGalinettes shouldBe 0
+        }
+
+        scenario("echoue sans chasseurs") {
+            val repository = PartieDeChasseRepositoryForTests()
+            val service = PartieDeChasseService(repository) { LocalDateTime.now() }
+            val chasseurs: Map<String, Int> = mapOf()
+            val terrainDeChasse = ("Pitibon sur Sauldre" to 3)
+
+            shouldThrow<ImpossibleDeDémarrerUnePartieSansChasseur> {
+                service.démarrer(
+                    terrainDeChasse,
+                    chasseurs
+                )
+            }
+            repository.savedPartieDeChasse shouldBe null
+        }
+
+        scenario("echoue avec un terrain sans galinettes") {
+            val repository = PartieDeChasseRepositoryForTests()
+            val service = PartieDeChasseService(repository) { LocalDateTime.now() }
+            val terrainDeChasse = ("Pitibon sur Sauldre" to 0)
+
+            shouldThrow<ImpossibleDeDémarrerUnePartieSansGalinettes> {
+                service.démarrer(
+                    terrainDeChasse,
+                    mapOf()
+                )
+            }
+            repository.savedPartieDeChasse shouldBe null
+        }
+
+        scenario("echoue si chasseur sans balle") {
+            val repository = PartieDeChasseRepositoryForTests()
+            val service = PartieDeChasseService(repository) { LocalDateTime.now() }
+            val chasseurs = mapOf(
+                ("Dédé" to 20),
+                ("Bernard" to 0)
+            )
+            val terrainDeChasse = ("Pitibon sur Sauldre" to 3)
+
+            shouldThrow<ImpossibleDeDémarrerUnePartieAvecUnChasseurSansBalle> {
+                service.démarrer(
+                    terrainDeChasse,
+                    chasseurs
+                )
+            }
+            repository.savedPartieDeChasse shouldBe null
         }
     }
 })
