60df9aa829eec6aa83d9d07dc12f87363908b80d
Yoan Thirion <yoan.thirion@gmail.com>
2023-11-16 21:42:59 +0100

    add pitest for kt

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,7 +1,10 @@
+import info.solidsoft.gradle.pitest.PitestPluginExtension
+
 plugins {
     kotlin("jvm") version "1.9.20"
     application
     jacoco
+    id("info.solidsoft.pitest") version "1.9.11"
 }
 
 group = "org.bouchonnois"
@@ -13,6 +16,7 @@ repositories {
 
 dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-pitest:1.2.0")
 }
 
 tasks.test {
@@ -33,6 +37,10 @@ tasks.jacocoTestReport {
     reports {
         xml.required.set(true)
     }
+}
+
+configure<PitestPluginExtension> {
+    targetClasses.set(listOf("bouchonnois.*"))
 }
 
 application {
